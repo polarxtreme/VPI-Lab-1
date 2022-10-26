6d0770649690778e188d964dbf849d6e513e836c
polarxtreme <yungmatoi@gmail.com>
2022-10-26 14:17:24 +0300

    task 2

---
+++ lab1_ex2/lab 1 ex 2/Devices.cs
@@ -0,0 +1,25 @@
+﻿class Device
+{
+    public string Model { get; set; }
+    public string Mhz { get; set; }
+    public string Ram { get; set; }
+    public string Hdd { get; set; }
+    public string Weight { get; set; }
+
+    public Device() { Model = "Undefined"; Mhz = "Undefined"; Ram = "Undefined"; Hdd = "Undefined"; Weight = "Undefined"; }
+    public Device(string m, string mh, string r, string h, string w) { Model = m; Mhz = mh; Ram = r; Hdd = h; Weight = w; }
+    public Device(Device obj) { Model = obj.Model; Mhz = obj.Mhz; Ram = obj.Ram; Hdd = obj.Hdd; Weight = obj.Weight; }
+    public Device Clon() { return new Device(this); }
+    public string Info
+    {
+        get
+        {
+            return $"Model: {Model}\n" +
+                $"CPU Frequency: {Mhz}\n" +
+                $"RAM Capacity: {Ram}\n" +
+                $"HDD Capacity: {Hdd}\n" +
+                $"Weight: {Weight}\n";
+
+        }
+    }
+}
---
+++ lab1_ex2/lab 1 ex 2/Program.cs
@@ -0,0 +1,24 @@
+﻿List<Device> devices = new List<Device>()
+{
+    new Device("MacBook Air M1", "1", "2", "3", "4"),
+    new Device("Acer Chromebook Spin 311", "1", "2", "3", "4"),
+    new Device("MacBook Air M2", "1", "2", "3", "4"),
+    new Device("Acer Aspire 5", "1", "2", "3", "4"),
+    new Device("Dell XPS 15", "1", "2", "3", "4")
+
+};
+
+foreach (var device in devices)
+{
+    Console.WriteLine(device.Info);
+}
+
+Console.WriteLine("--------------------------------------Removed obj 1 & 3 from the list--------------------------------------");
+
+devices.RemoveAt(1);
+devices.RemoveAt(3);
+
+foreach (var device in devices)
+{
+    Console.WriteLine(device.Info);
+}
